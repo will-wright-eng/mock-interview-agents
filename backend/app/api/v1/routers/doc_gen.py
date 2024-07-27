0c2aa42b79e50db91465310c1e3b683fc0b4e58e
william-cass-wright <william.cass.wright@gmail.com>
2024-07-26 23:55:01 -0700

    groq working

--- backend/app/api/v1/routers/doc_gen.py
+++ backend/app/api/v1/routers/doc_gen.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 from app.core.config import settings
+from app.prompts.doc_gen import rubric_gen_prompt, question_gen_prompt
 
 router = r = APIRouter(
     prefix="/doc_gen",
@@ -22,7 +23,6 @@ async def chat(request: ChatRequest):
     client = Groq(
         api_key=settings.GROQ_API_KEY,
     )
-
     chat_completion = client.chat.completions.create(
         messages=[
             {
@@ -32,5 +32,33 @@ async def chat(request: ChatRequest):
         ],
         model="llama3-8b-8192",
     )
-
     return ChatResponse(response=chat_completion.choices[0].message.content)
+
+
+class DocGenRequest(BaseModel):
+    type: str = "questions"
+
+
+@r.post("/generate_questions")
+async def generate_doc(request: DocGenRequest):
+    if request.type == "questions":
+        request_content = question_gen_prompt
+    elif request.type == "rubric":
+        request_content = rubric_gen_prompt
+    else:
+        raise ValueError(f"Invalid type: {request.type}")
+
+    client = Groq(
+        api_key=settings.GROQ_API_KEY,
+    )
+    questions_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": request_content,
+            },
+        ],
+        model="llama3-8b-8192",
+    )
+    questions = [choice.message.content for choice in questions_completion.choices]
+    return {"questions": questions}
